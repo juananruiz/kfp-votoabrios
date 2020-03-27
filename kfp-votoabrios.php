c4d0cb5a4cbaa1d2572d40a6abf979bbf4766844
Juanan Ruiz <juanan@us.es>
2020-03-27 19:51:10 +0100

    Quita require innecesario

--- kfp-votoabrios.php
+++ kfp-votoabrios.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:  KFP VotoABrios
  * Plugin URI:   https://github.com/kungfupress/kfp-votoabrios
- * Description:  Votaciones sobre una colección de imágenes, de momento de voto único [kfp_votoabrios]
+ * Description:  Votaciones sobre una colección de imágenes, de momento de voto único [kfp_votoabrios_obras]
  * Version:      0.1.0
  * Author:       KungFuPress
  * Author URI:   https://kungfupress.com/
@@ -21,12 +21,10 @@ define( 'KFP_VOTOABRIOS_VERSION', '0.1.0' );
 
 // Crea tabla.
 require_once KFP_VOTOABRIOS_DIR . 'include/crea-tablas.php';
-// Crea y rellena taxonomías.
-require_once KFP_VOTOABRIOS_DIR . 'include/crea-taxonomias.php';
-// Agrega shortcode [kfp_votoabrios] para mostrar formulario.
-require_once KFP_VOTOABRIOS_DIR . 'include/shortcode-form.php';
+// Agrega shortcode [kfp_votoabrios] para mostrar lista de obras.
+require_once KFP_VOTOABRIOS_DIR . 'include/shortcode-obras.php';
 // Agrega función para que admin-post.php capture el envío de un nuevo taller desde un formulario.
-require_once KFP_VOTOABRIOS_DIR . 'include/graba-form.php';
+require_once KFP_VOTOABRIOS_DIR . 'include/graba-voto.php';
 // Panel con lista de registros en el escritorio.
 require_once KFP_VOTOABRIOS_DIR . 'include/admin-menu.php';
 // Módulo para descargar las registros existentes.
