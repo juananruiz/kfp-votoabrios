9b84c27a3f360b21d4b434a71503ac86b4a8f839
Juanan Ruiz <juanan@us.es>
2020-03-27 19:06:24 +0100

    Corrige fallos en definición tablas

--- include/crea-tablas.php
+++ include/crea-tablas.php
@@ -7,31 +7,33 @@
 
 defined( 'ABSPATH' ) || die();
 
-register_activation_hook( KFP_VOTOABRIOS_PLUGIN_FILE, 'kfp_crea_tablas' );
+register_activation_hook( KFP_VOTOABRIOS_PLUGIN_FILE, 'kfp_votoabrios_crea_tablas' );
 /**
  * Cuando el plugin se active se crean las tablas si no existen.
  *
  * @return void
  */
-function kfp_crea_tablas() {
+function kfp_votoabrios_crea_tablas() {
 	global $wpdb;
-	$tabla_obra = $wpdb->prefix . '_votoabrios_obra';
-	$tabla_voto = $wpdb->prefix . '_votoabrios_voto';
+	$tabla_obra      = $wpdb->prefix . 'votoabrios_obra';
+	$tabla_voto      = $wpdb->prefix . 'votoabrios_voto';
 	$charset_collate = $wpdb->get_charset_collate();
 
-	$query  = "CREATE TABLE IF NOT EXISTS $tabla_obra (
+	$query1 = "CREATE TABLE IF NOT EXISTS $tabla_obra (
 		id int(11) NOT NULL AUTO_INCREMENT,
 		autor varchar(250),
 		descripcion text,
 		UNIQUE (id)
 		) $charset_collate;";
-	$query .= "CREATE TABLE IF NOT EXISTS $tabla_voto (
+	$query2 = "CREATE TABLE IF NOT EXISTS $tabla_voto (
 		id int(11) NOT NULL AUTO_INCREMENT,
 		obra_id int(11) NOT NULL,
 		ip varchar(15) NOT NULL,
+		UNIQUE (id)
 		) $charset_collate;";
 	// La función dbDelta que nos permite crear tablas de manera segura se
 	// define en el fichero upgrade.php que se incluye a continuación.
 	include_once ABSPATH . 'wp-admin/includes/upgrade.php';
-	dbDelta( $query );
+	dbDelta( $query1 );
+	dbDelta( $query2 );
 }
